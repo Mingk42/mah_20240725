31eecce6c1b2f4903908f552eec99287e1e89dc7
Mingk42 <xoals123456t@gmail.com>
2024-07-25 15:29:36 +0900

    copy code 2 fun

--- src/mingk42_args_history/db/utils.py
+++ src/mingk42_args_history/db/utils.py
@@ -1,11 +1,22 @@
 import pandas as pd
 
+def read_data(path="~/data/parquet"):
+    df = pd.read_parquet(path)
 
-def top():
-    df = pd.parquet("~/data/parquet")
-    fdf=df[df['dt']=='2024-07-17']
-    sdf = fdf.sort_values(by='cnt', ascending=False).head(10)
+    return df
+
+def top(dt,top):
+    df = read_data()
+    fdf=df[df['dt']==dt]
+    sdf = fdf.sort_values(by='cnt', ascending=False).head(top)
     ddf = sdf.drop(columns=['dt'])
 
     r = ddf.to_string(index=False)
     return r
+
+def count(query):
+    df = read_data()
+    fdf = df[df['cmd'].str.contains(query)]
+    cnt = fdf['cnt'].sum()
+
+    return cnt
